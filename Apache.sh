87e0e70d4668a638792c2de8eccec200bd2774a6
adedayo <ndespace360@gmail.com>
2022-05-25 23:42:17 -0400

    i added see you soon

--- Apache.sh
+++ Apache.sh
@@ -2,7 +2,7 @@
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Welcome to Adetechnologies LLC. Thanks for your patronage" > /var/www/html/index.html
+echo "Welcome to Adetechnologies LLC. Thanks for your patronage, see you soon" > /var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
 systemctl restart httpd
