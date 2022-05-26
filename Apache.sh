2efbf913cf1d62e2139c63e49db49c833449e148
ruth <ruthadediran24@gmail.com>
2022-05-25 23:34:50 -0400

    i removed the port

--- Apache.sh
+++ Apache.sh
@@ -4,7 +4,6 @@ systemctl start httpd
 systemctl enable httpd
 echo "Welcome to Adetechnologies LLC. Thanks for your patronage" > /var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
-firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
 systemctl restart httpd
 
