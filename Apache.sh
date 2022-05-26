df4a91362aae66ce5f1309a2cfbb1c0db70d25ba
Ade <tp2dyone@gmail.com>
2022-05-25 22:59:03 -0500

    i added welcome home

--- Apache.sh
+++ Apache.sh
@@ -2,7 +2,8 @@
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Welcome to Adetechnologies LLC. Thanks for your patronage, see you soon" > /var/www/html/index.html
+echo "Welcome to Adetechnologies LLC. Thanks for your patronage, Welcome Home" > /var/www/html/index.html
+echo "This is the Linux website" >> ?var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
 systemctl restart httpd
