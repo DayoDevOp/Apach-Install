f1d07596c2f7dacd080682a9f84c8e9a62403578
root <Isaac>
2022-08-12 23:37:05 -0400

    i added beautiful

--- Apache.sh
+++ Apache.sh
@@ -2,7 +2,7 @@
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Welcome to Adetechnologies LLC. Thanks for your patronage, Welcome Home Baby Isaac. RUTH" > /var/www/html/index.html
+echo "Welcome to Adetechnologies LLC. Thanks for your patronage, Welcome Home Baby Isaac. RUTH is beautiful" > /var/www/html/index.html
 echo "This is the Linux website" >> ?var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
