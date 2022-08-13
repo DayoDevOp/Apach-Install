b3ec03f19f68f6da8a0a4aa4645316700aa45ebb
server8 <server8@gmail.com>
2022-08-13 13:33:13 -0400

    i removed second echo line

--- Apache.sh
+++ Apache.sh
@@ -3,7 +3,6 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 echo "Welcome to Adetechnologies LLC. Thanks for your patronage, Welcome Home Baby Isaac. RUTH is beautiful and Godly" > /var/www/html/index.html
-echo "This is the Linux website. Test Jenkins" >> var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
 systemctl restart httpd
