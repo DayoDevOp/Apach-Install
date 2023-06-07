e20a0e02d2883f5d6c7aedcbf4414cf49c9a7af0
Adedayo Adediran <adedayoadediran99@gmail.com>
2023-06-06 21:41:08 -0400

    you AMEN

--- Apache.sh
+++ Apache.sh
@@ -2,7 +2,7 @@
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Welcome to Adetechnologies LLC. Thanks for your patronage, Welcome Home Baby Isaac. God bless us" > /var/www/html/index.html
+echo "Welcome to Adetechnologies LLC. Thank you for your patronage, Welcome Home Baby Isaac. God bless us AMEN" > /var/www/html/index.html
 firewall-cmd --permanent --add-service={http,https}
 firewall-cmd --reload
 systemctl restart httpd
